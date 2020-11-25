6fb8b7cdd7237ab571df51d41d91979777736615
Thomas O'Hara <thomas.e.ohara@protonmail.com>
2020-11-25 12:30:06 +0100

    Separated V3.0.0 AIP.py and update_data into main and module

--- AIP.py
+++ AIP.py
@@ -5,8 +5,9 @@ import operator
 from datetime import datetime
 import shutil
 import os
-from whitelist_module import load_whitelist, check_if_ip_is_in_whitelisted_nets, check_if_ip_is_in_whitelisted_ips
-import main_modulev3
+from Modules.whitelist_module import *
+import Modules.main_modulev3
+from Modules.update_historical_data import *
 
 startTime = datetime.now()
 
@@ -23,28 +24,8 @@ with open(file_for_functions, 'r') as file:
         else:
             list_of_functions_that_were_choosen.extend(line)
 
-functions_list = [o for o in getmembers(main_modulev3) if isfunction(o[1])]
-
-
-# >>>>>>>>> Needs to be here so it can be called immediately, fine what data
-# files have not been processed.
-def find_new_data_files(b, c):
-    list_of_new_data_files = []
-    list_of_data_files = os.listdir(b)
-    dictionary_of_dates_on_files = {}
-    with open(c, 'r') as record:
-        list_of_processed_data_files = record.read().split('\n')
-    for file in list_of_data_files:
-        if file not in list_of_processed_data_files:
-            list_of_new_data_files.extend([file])
-            dictionary_of_dates_on_files[file[0:10]] = file
-    for file12 in list_of_new_data_files:
-        with open(c, 'a') as records_file1:
-            records_file1.write(file12 + '\n')
-    sorted_dates = sorted(dictionary_of_dates_on_files, key=lambda date: datetime.strptime(date, '%Y-%m-%d'))
-    sorted_dates.reverse()
-    print(sorted_dates)
-    return list_of_new_data_files, sorted_dates[0]
+# Generate a list of available fully defined sorting functions
+functions_list = [o for o in getmembers(Modules.main_modulev3) if isfunction(o[1])]
 
 # Full path to directory where all the files will be stored
 # (a)
@@ -101,122 +82,6 @@ time_file = AIPP_direcory + '/Times.csv'
 path_aging_modifier_pc = AIPP_direcory + '/Aging-modifiers-pc.csv'
 path_aging_modifier_pn = AIPP_direcory + '/Aging-modifiers-pn.csv'
 
-def open_sort_new_file(b, list_of_new_files):
-    list_of_new_data_flows = []
-    list_of_IPs_in_new_data = []
-    for file in list_of_new_files:
-        with open(b + '/' + file, 'r') as csvfile:
-            for line in csv.reader(csvfile):
-                if line[0] != 'SrcAddr':
-                    list_of_new_data_flows.append([line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9]])
-                    list_of_IPs_in_new_data.append(line[0])
-                else:
-                    continue
-    return list_of_new_data_flows, list_of_IPs_in_new_data
-
-
-def open_sort_abs_file(e):
-    IP_flows = []
-    IPs_in_absolute_file = []
-    with open(e, 'r') as csvfile:
-        for line in csv.reader(csvfile):
-            if not line:
-                break
-            else:
-                IP_flows.append([line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10]])
-                IPs_in_absolute_file.append(line[0])
-    return IP_flows, IPs_in_absolute_file
-
-def get_updated_flows(location_of_absolute_data_file):
-    IP_flows = []
-    with open(location_of_absolute_data_file, 'r') as csvfile:
-        for line in csv.reader(csvfile):
-            if not line:
-                break
-            else:
-                IP_flows.append([line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10]])
-    return IP_flows
-
-def sort_IPs_from_data(IPs_from_absolute_data, IP_flows_from_todays_data):
-    unknown_IP_flows = []
-    unknown_IPs = []
-    known_IPs = []
-    known_IP_data_flows = []
-    for IP in IP_flows_from_todays_data:
-        if IP[0] in IPs_from_absolute_data:
-            known_IP_data_flows.append(IP)
-            known_IPs.append(IP[0])
-        elif IP[0] not in IPs_from_absolute_data:
-            unknown_IP_flows.append(IP)
-            unknown_IPs.append(IP[0])
-    return unknown_IP_flows, unknown_IPs, known_IP_data_flows, known_IPs
-
-
-def write_unkown_IPs_to_data_file(list_of_unknown_IPs, d):
-    with open(d, 'a') as data_file:
-        for flow in list_of_unknown_IPs:
-            data_file.write(flow + '\n')
-
-
-def update_records_files(e, list_of_known_new_IP_data, unknown_IP_flows):
-    absolute_data, IPs_in_abs_file = open_sort_abs_file(e)
-    new_absolute_file_flows = []
-    new_absolute_file_flows += absolute_data
-    unknown_IP_flows_new = []
-    for new_flow2 in unknown_IP_flows:
-        new_flow2.extend([new_flow2[1]])
-        unknown_IP_flows_new.append(new_flow2)
-    new_absolute_file_flows.extend(unknown_IP_flows_new)
-
-    if not list_of_known_new_IP_data:
-        pass
-
-    else:
-        for x1, new_flow in enumerate(list_of_known_new_IP_data):
-            for x2, absolute_flow in enumerate(new_absolute_file_flows):
-                if absolute_flow[0] == new_flow[0]:
-                    days_since_first_seen = (current_time - float(absolute_flow[9])) // 86400.0
-                    if days_since_first_seen != 0:
-                        updated_event_average = ((float(absolute_flow[10])) * (days_since_first_seen - 1) + float(
-                            new_flow[1])) / days_since_first_seen
-                    else:
-                        updated_event_average = ((float(absolute_flow[10])) * (days_since_first_seen - 1) + float(
-                            new_flow[1])) / 1
-
-                    updated_total_events = float(new_flow[1]) + float(absolute_flow[1])
-                    updated_total_duration = float(absolute_flow[2]) + float(new_flow[2])
-                    updated_average_duration = (float(absolute_flow[3]) + float(new_flow[3])) / 2.0
-                    updated_total_bytes = float(absolute_flow[4]) + float(new_flow[4])
-                    updated_average_bytes = (float(absolute_flow[5]) + float(new_flow[5])) / 2.0
-                    updated_total_packets = float(absolute_flow[6]) + float(new_flow[6])
-                    updated_average_packets = (float(absolute_flow[7]) + float(new_flow[7])) / 2.0
-                    updated_last_event = new_flow[9]
-
-                    updated_entry = [new_flow[0], updated_total_events, updated_total_duration,
-                                     updated_average_duration,
-                                     updated_total_bytes, updated_average_bytes, updated_total_packets,
-                                     updated_average_packets,
-                                     absolute_flow[8], updated_last_event, updated_event_average]
-                    new_absolute_file_flows[x2] = updated_entry
-                    break
-                else:
-                    continue
-
-    whitelisted_nets, whitelisted_ips = load_whitelist()
-    for index, flow in enumerate(new_absolute_file_flows):
-        judgement1 = check_if_ip_is_in_whitelisted_nets(flow[0], whitelisted_nets)
-        judgement2 = check_if_ip_is_in_whitelisted_ips(flow[0], whitelisted_ips)
-        if (judgement1==True) or (judgement2==True) is True:
-            del new_absolute_file_flows[index]
-            print('Found ', flow[0], ' in Whitelisted IPs. Deleting entry...')
-        else:
-            continue
-
-    with open(e, 'w') as new_file_another:
-        wr2 = csv.writer(new_file_another, quoting=csv.QUOTE_ALL)
-        for y in new_absolute_file_flows:
-            wr2.writerow(y)
-
 
 def sort_data_decending(data):
     list_as_dictionary = {}
@@ -238,7 +103,6 @@ write_unkown_IPs_to_data_file(unknown_IPs_from_new_data, record_file_path_to_kno
 
 update_records_files(record_file_path_for_absolute_data, known_IP_data_flows_from_new_data, unknown_IP_flows_from_new_data)
 
-# new_absolute_file_data = get_updated_flows(record_file_path_for_absolute_data)
 
 number_of_lines = len(open(record_file_path_for_absolute_data).readlines())
 print(number_of_lines)
@@ -248,7 +112,7 @@ def create_final_blacklist(path_to_file, data_from_absolute_file, function_to_us
         write2 = csv.writer(new_file2, quoting=csv.QUOTE_ALL)
         write2.writerow(('# Top IPs from data gathered in last 24 hours only', date))
         write2.writerow(('Number', 'IP address', 'Rating'))
-        if function_to_use == getattr(main_modulev3, list_of_functions_that_were_choosen[1]):
+        if function_to_use == getattr(Modules.main_modulev3, list_of_functions_that_were_choosen[1]):
             print('using pn')
             for x2, interesting_rating2 in enumerate(sort_data_decending(function_to_use(data_from_absolute_file, current_time, path_aging_modifier_pn))):
                 if float(interesting_rating2[1]) >= 0.00008:
@@ -259,7 +123,7 @@ def create_final_blacklist(path_to_file, data_from_absolute_file, function_to_us
                     write2.writerow(new_list2)
                 else:
                     break
-        elif function_to_use == getattr(main_modulev3, list_of_functions_that_were_choosen[0]):
+        elif function_to_use == getattr(Modules.main_modulev3, list_of_functions_that_were_choosen[0]):
             print('using pc')
             for x2, interesting_rating2 in enumerate(sort_data_decending(function_to_use(data_from_absolute_file, current_time, path_aging_modifier_pc))):
                 if float(interesting_rating2[1]) >= 0.0004:
@@ -284,9 +148,9 @@ def create_final_blacklist(path_to_file, data_from_absolute_file, function_to_us
 # Pull the three functions that were choosen by the user from the dictionary of functions.
 # print(list_of_functions_that_were_choosen)
 
-PCF = getattr(main_modulev3, list_of_functions_that_were_choosen[0])
-PNF = getattr(main_modulev3, list_of_functions_that_were_choosen[1])
-OTF = getattr(main_modulev3, list_of_functions_that_were_choosen[2])
+PCF = getattr(Modules.main_modulev3, list_of_functions_that_were_choosen[0])
+PNF = getattr(Modules.main_modulev3, list_of_functions_that_were_choosen[1])
+OTF = getattr(Modules.main_modulev3, list_of_functions_that_were_choosen[2])
 
 # Call the create blacklist function for each of the three user input functions
 create_final_blacklist(top_IPs_for_all_time, get_updated_flows(record_file_path_for_absolute_data), PCF)
