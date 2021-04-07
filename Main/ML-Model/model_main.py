90d40d5e089715960c80c5b8352282c433f0c266
Thomas O'Hara <thomas.e.ohara@protonmail.com>
2021-04-07 14:10:41 +0200

    added testing file for ml-model

---
+++ Main/ML-Model/model_main.py
@@ -0,0 +1,56 @@
+import os
+import csv
+import datetime
+import argparse
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+import numpy as np
+from datetime import datetime
+import time
+from load_data import *
+from train_model import *
+
+input_data = '/home/the-shadow/Data/Labeled-Data/2021-02-01_labelled_raw.csv'
+prediction_data = '/home/the-shadow/Data/Labeled-Data/2021-02-02_labelled_raw.csv'
+
+# Process the training data
+data = load_data(input_data)
+data = add_row(data)
+y_all, X_all = separate_labels_data(data)
+X_train, X_test, y_train, y_test = bin_data(y_all, X_all)
+
+# Process the data we will be predicting on
+no_label_data = load_data(prediction_data)
+no_label_data = add_row(no_label_data)
+y_all_pred, X_all_pred = separate_labels_data(data)
+list_of_new_data_flows, list_of_IPs_in_new_data = open_sort_new_file(prediction_data)
+
+# Train a model and find good paramaters
+best_params = find_best_param(X_train, X_test, y_train, y_test)
+
+# Train a model using best params, using the whole dataset this time
+predictions = train_on_complete_data(X_all, y_all, X_all_pred, best_params)
+
+print('predictions: ', predictions)
+print('list_of_new_data_flows: ', list_of_new_data_flows)
+
+blacklist = create_blacklist(predictions, list_of_new_data_flows)
+
+print('Number of BL IPs: ', len(blacklist))
+
+
+
+
+
+
+
+
+
+
+
