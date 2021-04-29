7e573971af065770986b16448bf57e0b92c500d7
Thomas O'Hara <thomas.e.ohara@protonmail.com>
2021-04-29 10:56:35 +0200

    added line dropping file

--- Main/AIP_RF.py
+++ Main/AIP_RF.py
@@ -202,6 +202,8 @@ else:
             lines_to_drop = len(combined_data)//30
             combined_data.drop(pd.Series(np.arange(0, lines_to_drop, 1)))
             print("Dropped", lines_to_drop, ' lines')
+            with open(AIP_output_data_directory + '/ML_Model_Data/dropped_lines.txt', "a") as myfile:
+                myfile.write(str(date) + ": Dropped" + str(lines_to_drop) + ' lines' + ': ' + str(len(combined_data)) + ' lines remaining')
         else:
             print("Days in Data: ", days_in_historical_data)
 
